cfdf2e3598475df4c7f2b4b4c198bbaf4ae33c06
Srichardson2713 <112520173+Srichardson2713@users.noreply.github.com>
2022-08-30 21:36:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+your_name = input("What is your name? ")
+place = input("Name a place: ")
+friends_name = input("What is your friends name? ")
+body_part = input("Name a body part (Plural): ")
+past_tense_action = input("Write a Past-Tense Action.. for example {kicked}: ")
+interjection = input("Name and interjection.. for example {Wow!} ")
+
+
+
+print(your_name + " woke up early in the morning and went to " + place + " to grab a drink")
+print("When he arrived, he ran into " + friends_name + " and saw " + friends_name + " looking at pictures of " + body_part)
+print(your_name + " then asked " + friends_name + " why he liked " + body_part + " so much ")
+print(friends_name + " said, well, I think it was because I was never " + past_tense_action + " as a child")
+print(interjection + "! Well im calling the cops you absolute freak lord.")
+print("The End")
